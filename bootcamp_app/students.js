6a92f250666342eee9d170b0b92360676be32fc8
Sean Sullivan <dev@seanmedia.ca>
2020-04-30 03:37:04 +0000

    Refactor to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -3,13 +3,19 @@ const options = require('./secret.json');
 
 const pool = new Pool(options);
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE 
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${process.argv[2]}%`, limit];
+  
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -3,15 +3,19 @@ const options = require('./secret.json');
 
 const pool = new Pool(options);
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const limit = process.argv[2] || 'JUL02';
+
+pool.query(queryString, [limit])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
